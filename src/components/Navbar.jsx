2d12a8bd78378c04fcfe9fead0a6c94f51fc9bfb
EdHuayllasco <edwardhc1997@gmail.com>
2023-08-31 18:41:41 -0500

    feat: :sparkles: Configura Links de navegación

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,86 @@
-import { useState } from 'react';
-import { Container, Nav, Navbar as NavbarBS, NavDropdown} from "react-bootstrap";
+import { 
+  Button,
+  Container, 
+  Nav, 
+  Navbar as NavbarBS, 
+  NavLink,
+  Offcanvas
+} from "react-bootstrap";
+
 import { CartWidget } from './CartWidget';
+import { MenuIcon } from "./Icons";
+import '../assets/styles/main.css';
+import { useState } from "react";
 
 
 export const Navbar = () => {
 
-  const [cartQuantity, setCartQuantity] = useState(1);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   return (
 
     <NavbarBS bg="dark" data-bs-theme="dark">
       <Container>
-        <NavbarBS.Brand href="#home">4everStore</NavbarBS.Brand>
-        <Nav className="me-auto">
-          <NavDropdown title="Categorías" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Ropa Mujer</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action4">Ropa Hombre</NavDropdown.Item>
-            <NavDropdown.Divider />
-
-         
-          </NavDropdown>
-          <Nav.Link href="/" >
-            Home
+        <NavbarBS.Brand href="/">4everStore</NavbarBS.Brand>
+        <Nav className="me-auto vertical-menu__container">
+          
+          <Nav.Link href = "/" as = { NavLink }>
+            ALL
           </Nav.Link>
-          <Nav.Link href="/store" >
-            Store
+          <Nav.Link href = "/category/women's clothing" as = { NavLink }>
+            WOWEN
           </Nav.Link>
-          <Nav.Link href="/about" >
-            About
+          <Nav.Link href = "/category/men's clothing" as = { NavLink }>
+            MEN
+          </Nav.Link>
+          <Nav.Link href = "/category/electronics" as = { NavLink }>
+            ELECTRONIC
+          </Nav.Link>
+          <Nav.Link href = "/category/jewelery" as = { NavLink }>
+            JEWELERY
           </Nav.Link>
         </Nav>
-        <CartWidget cartQuantity = { cartQuantity }/>
+        <div>
+          <CartWidget />
+          <button 
+            className = "btn menu-button"
+            onClick = { handleShow }>
+              <MenuIcon/>
+          </button>
+        </div>
       </Container>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>
+            <NavbarBS.Brand href="/">4everStore</NavbarBS.Brand>
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        <Nav className="me-auto">
+          
+          <Nav.Link href = "/" as = { NavLink }>
+            ALL
+          </Nav.Link>
+          <Nav.Link href = "/category/women's clothing" as = { NavLink }>
+            WOWEN
+          </Nav.Link>
+          <Nav.Link href = "/category/men's clothing" as = { NavLink }>
+            MEN
+          </Nav.Link>
+          <Nav.Link href = "/category/electronics" as = { NavLink }>
+            ELECTRONIC
+          </Nav.Link>
+          <Nav.Link href = "/category/jewelery" as = { NavLink }>
+            JEWELERY
+          </Nav.Link>
+        </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
     </NavbarBS>
    
   )
