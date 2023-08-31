a7c0beec53f57e84a111de51a87fc7fdd5190026
EdHuayllasco <edwardhc1997@gmail.com>
2023-08-31 18:53:02 -0500

    feat: :sparkles: Muestra datos de un producto en la pagina del carro de compras

---
+++ src/components/CartControlButtons.jsx
@@ -0,0 +1,40 @@
+import { Button } from "react-bootstrap"
+import { useCart } from "../context/CartContext";
+
+export const CartControlButtons = ({ item }) => {
+
+    const {
+        id,
+        quantityItem
+    } = item;
+      
+    const {
+        addItem,
+        removeItem,
+    } = useCart();
+
+
+    return (
+        <div
+            className="d-flex align-items-center justify-content-start"
+            style={{ gap: ".5rem"}}
+        >
+            <Button
+                variant="outline-primary" 
+                onClick={() => removeItem(id)}
+            >
+                -
+            </Button>
+            <div>
+                <span className="fs-5">{quantityItem} in cart </span> 
+            </div>
+            <Button 
+                variant="outline-primary" 
+                onClick={() => addItem(id, {})}
+            >
+                +
+            </Button>
+
+        </div>
+    )
+}
