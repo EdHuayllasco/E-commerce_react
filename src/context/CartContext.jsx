d2771ea2405f2b3a809708b0a7ec10a1be2c710f
EdHuayllasco <edwardhc1997@gmail.com>
2023-08-31 18:50:11 -0500

    feat: :sparkles: Crea contexto para el carrito de compras
    - agrega o remueve elementos del carrito de compras. -vacia completamente el carrito. -retorna la cantidad de un elemento

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,79 @@
+import { createContext, useContext, useState } from "react";
+
+
+const CartContext = createContext();
+
+export const useCart = () => {
+    return useContext(CartContext)
+};
+
+export const CartProvider = ({ children }) => {
+
+    const [cartItems, setCartItems] = useState([]);
+
+    const totalPriceCart = cartItems.reduce((totalPrice, item) => {
+        return totalPrice + item.quantity * item.price;
+    }, 0);
+
+    const quantityItems =  cartItems.reduce((quantity, item) => quantity + item.quantity,0);
+      
+  
+    const deleteItem = (id) => {
+        setCartItems( items => {
+            return items.filter(item => item.id !== id)
+        })
+    };
+
+     const addItem = (id, details ) => {
+        setCartItems( items => {
+            if(items.find(item => item.id === id) == null ){
+                return [ ...items, { id, quantity: 1, ...details }]
+            } else {
+                return items.map(item => {
+                    if(item.id === id) {
+                        return {...item, quantity: item.quantity + 1 }
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    };
+
+      const removeItem = (id) => {
+        setCartItems( items => {
+            if(items.find(item => item.id === id)?.quantity === 1 ){
+                return items.filter((item) => item.id !== id)
+            } else {
+                return items.map(item => {
+                    if(item.id === id) {
+                        return {...item, quantity: item.quantity - 1, ...item.details }
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    };
+
+    const clearCart = () => setCartItems([]);
+
+    const getQuantityItemById = (id) => {
+        return cartItems.find((item) => item.id === id)?.quantity || 0;
+    };
+
+    return (
+        <CartContext.Provider value={{
+            totalPriceCart,
+            cartItems,
+            quantityItems,
+            deleteItem,
+            addItem,
+            removeItem,
+            getQuantityItemById,
+            clearCart
+        }}>
+            {children}
+        </CartContext.Provider>
+    )
+};
