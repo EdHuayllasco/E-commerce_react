4c163a8709cd6cf676d9da89d73f81b3ce951dd7
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-27 18:26:35 -0500

    get category collection from firestores

---
+++ src/firebase/category.js
@@ -0,0 +1,31 @@
+
+import { db } from './firebaseConfig';
+import { 
+    collection, 
+    query, 
+    getDocs, 
+} from "firebase/firestore";
+
+
+const getCategories = async () => {
+    
+    const data = [];
+    const documents = query(collection(db, "categories"));
+    
+    const querySnapshot = await getDocs(documents);
+    querySnapshot.forEach((doc) => {
+        const documento = doc.data();
+        data.push({...documento, id: doc.id});
+    });
+
+    return data;
+}
+
+
+
+
+export {
+    getCategories
+}
+
+
