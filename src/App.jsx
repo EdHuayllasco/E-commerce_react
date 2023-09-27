9ee1618c9bb981cabefcc91e7de907743cd5089e
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-26 19:41:51 -0500

    Delete route components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import { Navbar } from './components/Navbar'
-import { ItemListContainer } from './pages/ItemListContainer'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { ItemDetailContainer } from './pages/ItemDetailContainer'
 import { CartProvider } from './context/CartContext'
-import { CartContainer } from './pages/CartContainer'
+import { MainRoutes } from './routes/MainRoute'
+
 
 function App() {
 
   return (
     <CartProvider>
-      <Navbar/>
-      <Routes>
-        <Route
-          path = '/'
-          element = { <ItemListContainer/>}
-        />
-        <Route
-          path = 'category/:categoryId'
-          element = { <ItemListContainer/>}
-        />
-        <Route
-          path = 'item/:itemId'
-          element = { <ItemDetailContainer/>}
-        />
-         <Route
-          path = 'cart/'
-          element = { <CartContainer/>}
-        />
-         <Route path="*" element = { <Navigate to="/" replace /> } /> {/* Ruta para manejar rutas no encontradas */}
-      </Routes>
+      <MainRoutes/>
     </CartProvider>
   )
 }
