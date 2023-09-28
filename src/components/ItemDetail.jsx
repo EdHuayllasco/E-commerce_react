067b1b8d418f80677333b60dec51fec19b873f0e
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-27 21:50:43 -0500

    Show more details of an Item

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,120 @@
+import '../assets/styles/components/itemDetail.css';
+import { Button, Image } from "react-bootstrap";
+import { Counter } from "./Counter";
+import { useState } from "react";
+
+export const ItemDetail = ({ data, addItem }) => {
+
+  
+  const { 
+    name, 
+    description,
+    details,
+    images, 
+    price,
+    discount,
+    id
+  } = data;
+
+  const [quantityToIncrement, setQuantityToIncrement] = useState(1);
+
+  const [imageSelected, setImageSelected] = useState({
+    image: images[0],
+    index: 0
+  });
+
+  const onChangeImage = (index) => {
+    setImageSelected({
+      image: images[index],
+      index
+    })
+  }
+
+  const imagesQuantity = images.length;
+  const priceWithDiscount = price - ((price * discount) / 100);
+  
+
+  const elementos = [];
+    for (const atrr in details) {
+      if (details.hasOwnProperty(atrr)) {
+        elementos.push(
+          <li key={atrr}>
+            <strong>{atrr}:</strong>  {details[atrr]}
+          </li>
+        );
+      }
+    }
+
+  return (
+    <>
+      <div className = "product-container my-3">
+        <div className="product-image-container shadow-sm p-3 m-1 rounded-4">
+          <Image 
+            src={imageSelected.image}
+            className="image-displayed mb-3"/>
+          <div className="images-list-container">
+            {
+              images.map((img, index) => 
+                <Image 
+                  src={img}
+                  onClick={() => onChangeImage(index)}
+                  key={`${id}-img-${index}`}
+                  style={{ width: `${100/imagesQuantity}%`, maxWidth: '100px', maxHeight: "100px"}} 
+                  className={`image-list ${(index == imageSelected.index) ? 'image-selected': ''}`}/>
+              )
+            }
+          </div>
+        </div>
+        <div className="product-details-container shadow-sm m-1 rounded-4">
+          <h4 className="product-title">{name}</h4>
+          <hr />
+          <ul className="product-details">
+            {
+              description.map((des, index) => <li key={`${id}-des-${index}`}>{des}</li> )
+            }
+          </ul>
+          <hr />
+          <div className="price-container"> 
+            <h4 className="discounted-price">
+              S/ {Math.ceil(priceWithDiscount)} 
+            </h4>
+            <h4 className="total-price">S/ {price}</h4>
+            <h6 className="discount py-1 px-3 rounded-3">off {discount}%</h6>
+          </div>
+          <div className = "counter-container" >
+              <Counter 
+                quantity = {quantityToIncrement}
+                setQuantity={ setQuantityToIncrement }
+                />
+              <Button
+                className="bt main-btn rounded-3"
+                onClick={() => addItem(id, 
+                  {
+                    title: name ,
+                    price,
+                    priceWithDiscount: Math.ceil(priceWithDiscount),
+                    image: imageSelected.image,
+                    discount
+                  }, quantityToIncrement
+                )}
+              >
+                  Agregar al carrito
+              </Button>
+              {/* <FavoriteBtn productId={id}/> */}
+          </div>
+        </div>
+      </div>
+      <div className = "product-description-container rounded-4 shadow-sm m-1">
+        <h4 className="description-title">Descripción</h4>
+        <hr />
+        <ul>
+          {
+            elementos
+          }
+        </ul>
+       
+      </div>
+
+    </>
+  )
+}
--- src/pages/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer.jsx
@@ -1,65 +1,37 @@
 import { Container, Row, Col, Image, Button} from "react-bootstrap"
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import { CartControlButtons } from "../components/CartControlButtons";
 import { NotFound } from "../components/NotFound";
+import { useFirestore } from "../hooks/useFirestore";
+import { getProductById } from "../firebase/items";
+import { Loading } from "../components/Loading";
+import { ItemDetail } from "../components/ItemDetail";
 
 export const ItemDetailContainer = () => {
 
-  const { state } = useLocation();
-
-  if (!state) return <NotFound/>;
-
-  const {
-    description,
-      price,
-      title,
-      image,
-      id
-  } = state;
-
+  const { itemId } = useParams();
+  const { data, isLoading, error } = useFirestore(getProductById, itemId);
 
   const { 
     getQuantityItemById,
     addItem
   } = useCart();
 
-  const quantityItem = getQuantityItemById(id);
-
-
+  const quantityItem = getQuantityItemById(itemId);
+  
   return (
-    <Container  className = "py-4 mt-3 d-flex detail-item__container">
-      <Col className=" d-flex justify-content-center">
-        <Image src={image} style={{ width: '60%'}}/>
-      </Col>
-      <Col className="p-5">
-        <h4>{title}</h4>
-        <p>{ description }</p>
-
-        <h3>{price} $</h3>
-        <div className="d-flex w-100 justify-content-between gap-3" >
-          {
-            quantityItem > 0
-            ? <CartControlButtons item = {{
-                quantityItem,
-                title,
-                price,
-                image,
-                id,
-              }}/>
-            : <Button
-                className="w-100"
-                onClick = { 
-                  () => addItem(id, {
-                    title, price, image, description
-                  })}
-                  > 
-            Add to cart 
-          </Button>
-          }
-        </div>
-      </Col>
-    </Container>
-    
+    <>
+      {
+        (isLoading)
+          ? <Loading/>
+          : (!isLoading && !data)
+            ? <NotFound/>
+            : <ItemDetail 
+                data={data}
+                quantityItem={quantityItem}
+                addItem={addItem}
+              />
+      }
+    </>
   )
 }
