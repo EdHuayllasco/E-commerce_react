d126b328a2b016cd46845dded18b119dd8282ea4
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 16:01:23 -0500

    add new pages for cart routes

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,72 @@
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { Button, Stack } from "react-bootstrap";
+import { CartItem } from "../components/CartItem";
+
+import '../assets/styles/pages/cart.css'; 
+
+export const Cart = () => {
+  const { 
+      cartItems, 
+      totalPriceCart,
+      clearCart,
+      quantityItems
+  } = useCart();
+
+  const navigate = useNavigate();
+
+  return (
+      <div className="p-3">
+          {
+              (quantityItems > 0)
+              ?  <>
+                  <div className = 'cart-container d-flex gap-3' >
+                      <div  className = 'my-3'>
+                          <h4>Carrito <small>({quantityItems} productos)</small></h4>
+                          <Stack gap={3}  className="w-100">
+                              {
+                                  cartItems.map(item => {
+                                      return (
+                                          <CartItem
+                                            key={item.id} 
+                                                {...item }
+                                            />
+                                  )})
+                              }
+                          </Stack>
+                      </div>
+                  </div>
+                  <hr />
+                  <div className = 'cart-purchase-summary'>
+                      <div className="total-price-container">
+                          <h5 className='fw-bold'>Total</h5> 
+                          <h5 className='fw-bold'>S/ {totalPriceCart}</h5>
+                      </div>
+                      <div className="checkout-btn-container">
+                          <button 
+                            className='bt secundary-btn '
+                            onClick={clearCart}
+                          >
+                            Vaciar carrito
+                          </button>
+                          <Button 
+                            className="bt main-btn"
+                            onClick = { () => navigate('/cart/checkout')}
+                          >
+                            Continuar al checkout
+                          </Button>
+                      </div>
+                  </div>
+              </>
+              : <div className = 'cart-empty-container'>
+                  <h4>Tu carrito está vacío</h4> 
+                  <p>Para continuar comprando, selecciona una categoría, o busca por tu producto.</p>
+                  <Button className="btn main-btn" onClick = { () => navigate('/')}>
+                      Elige un producto
+                  </Button>
+                </div>
+          }
+
+    </div>
+  )
+};
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { createOrder } from "../firebase/orders";
+// import { CheckoutForm } from "../components/CheckoutForm";
+// import { PurchaseSummary } from "../components/PurchaseSummary";
+
+export const Checkout = () => {
+  const { 
+      cartItems,
+      totalPriceCart,
+      clearCart
+  } = useCart();
+  const navigate = useNavigate();
+
+  const generateOrder = (values) => {
+    console.log(values);
+
+    const currentDate = new Date();
+    const newOrder = {
+      date: currentDate.toLocaleString(),
+      state: 'generated',
+      items: cartItems,
+      totalPriceOrder: totalPriceCart
+    }
+    const order = createOrder(newOrder, values.email)
+    if(order) {
+      navigate('/cart/confirmation');
+      clearCart();
+
+    }
+  }
+
+  return (
+    <div className="checkout-container">
+      {/* <CheckoutForm
+        generateOrder =  {generateOrder}
+      />
+      <PurchaseSummary/> */}
+    </div>
+  )
+}
