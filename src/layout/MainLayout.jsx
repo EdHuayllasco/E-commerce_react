2ed83470293f8546f06b82911c6331f78b3a8e86
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 14:12:12 -0500

    Main layout to show navbar and all routes

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import {Navbar} from '../components/Navbar';
+import { getCategories } from '../firebase/category';
 
 const MainLayout = ({children}) => {
+
+  const [categories, setCategories] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    getCategoriesFirebase();
+  }, [])
+
+  const getCategoriesFirebase = async() => {
+    const data = await getCategories();
+    setCategories(data)
+  }
+  
   return (
     <>
-        <Navbar/>
-        <div className="container">
+        <Navbar categories = {categories}/>
+        <div className="main-container">
             {children}
         </div>
     </>
