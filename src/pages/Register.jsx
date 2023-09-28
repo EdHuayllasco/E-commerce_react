43e8c3ddc19939189e4bdd9f0b3dc3f713379baf
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 16:59:58 -0500

    feat: Add register form

---
+++ src/pages/Register.jsx
@@ -0,0 +1,117 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { FormGroup, FormControl, FormLabel } from 'react-bootstrap';
+import * as yup from 'yup';
+import { useAuth } from '../context/AuthContext';
+
+export const Register = () => {
+
+  const { setForm, registerUser } = useAuth();
+
+  const schema = yup.object().shape({
+    firstName: yup.string().required("Sus nombres son requeridos"),
+    lastName: yup.string().required("Sus apellidos son requeridos"),
+    email: yup.string().email('Correo electrónico inválido').required('El correo electrónico es requerido'),
+    password: yup.string().required('La contraseña es requerida')
+  });
+
+  
+
+  return (
+    <div>
+      <Formik
+        validationSchema={schema}
+        onSubmit={(values) => {
+          registerUser(values)
+        }}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+        }}
+      >
+        {({ handleSubmit, handleChange, values, touched, errors }) => (
+          <Form onSubmit={handleSubmit}>
+              <FormGroup className="mb-2" >
+                <FormLabel>Nombres</FormLabel>
+                <Field
+                  type="text"
+                  name="firstName"
+                  value={values.firstName}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.firstName && !errors.firstName}
+                  isInvalid={!!errors.firstName}
+                />
+                <ErrorMessage 
+                  name='firstName'
+                  component="div"
+                  className='text-danger'
+                />
+              </FormGroup>
+              <FormGroup className="mb-2" >
+                <FormLabel>Apellidos</FormLabel>
+                <Field
+                  type="text"
+                  name="lastName"
+                  value={values.lastName}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.lastName && !errors.lastName}
+                  isInvalid={!!errors.lastName}
+                />
+                <ErrorMessage 
+                  name='lastName'
+                  component="div"
+                  className='text-danger'
+                />
+              </FormGroup>
+              <FormGroup className="mb-2">
+                <FormLabel>Correo Electrónico</FormLabel>
+                <Field
+                  type="text"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  as={FormControl}
+                  isValid={touched.email && !errors.email}
+                  isInvalid={!!errors.email}
+                  />
+                <ErrorMessage 
+                  name='email'
+                  component="div"
+                  className='text-danger'
+                  />
+              </FormGroup>
+              <FormGroup className="mb-2">
+                <FormLabel>Contraseña</FormLabel>
+                <Field
+                  type="password"
+                  name="password"
+                  value={values.password}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.password && !errors.password}
+                  isInvalid={!!errors.password}
+                />
+
+                <ErrorMessage
+                  name='password'
+                  component="div"
+                  className='text-danger'
+                  />
+              </FormGroup>
+              <FormGroup className="mb-2">
+                <span>¿Ya tienes una cuenta? 
+                  <a href='#' onClick={() => setForm('login')}>
+                     Inciar sesión
+                  </a>
+                </span>
+              </FormGroup>
+              <button type="submit" className='bt main-btn'>Crear cuenta</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
