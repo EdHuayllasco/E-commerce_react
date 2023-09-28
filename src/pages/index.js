6657d84eab004c2e611315f9a8a2900cfd13556a
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 17:28:08 -0500

    feat: create a index file for pages and firebase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 export * from './Home';
 export * from './ItemDetailContainer';
 export * from './ItemListContainer';
+export * from './Cart';
+export * from './Checkout';
+export * from './Confirmation';
+
