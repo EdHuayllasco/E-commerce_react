64cc0e6c901dfe3108b0e75d3cf8a984d1295ed9
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 16:06:59 -0500

    feat:add a steps component to the purchase process

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,61 @@
+// import {Breadcrumb as Bc } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
+import '../assets/styles/components/breadCrumb.css';
+
+export const Breadcrumb = () => {
+
+  const location = useLocation();
+  const currentPathname = location.pathname;
+
+  const steps = [
+    {
+      path: '/cart',
+      name: 'Carrito',
+      next: true
+    },
+    {
+      path: '/cart/checkout',
+      name: 'Checkout',
+      next: true
+    },
+    {
+      path: '/cart/confirmation',
+      name: 'Confirmación',
+      next: false
+    }
+   
+  ]
+
+
+  const indexStep = steps.findIndex(step => step.path == currentPathname )
+
+
+  const style = (index) => {
+    if(index < indexStep) return 'bc-active';
+    else if (index == indexStep) return 'bc-current'
+    return 'bc-inactive'
+  } 
+
+
+  return (
+
+    <div className='breadcrumb-container'>
+      <ol className='breadcrumb-list'>
+        {
+          steps.map((step, index) => (
+            <li key = {`${index}-${step.name}`} className='breadcrumb-li'>
+              <Link 
+                to = {index < indexStep ? step.path : null}
+                className={`breadcrumb-link ${style(index)}`}
+              >
+                {step.name} 
+              </Link>
+              {step.next && <span className='breadcrumbs-separator'> {"  >  "} </span> }
+              
+            </li>
+          ))
+        }
+      </ol>
+    </div>
+  );
+}
