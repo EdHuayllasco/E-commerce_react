176e71ca8053e2e51bd57c14501f7313a00cefb7
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 17:03:18 -0500

    feat: Show purchase summary

---
+++ src/components/PurchaseSummary.jsx
@@ -0,0 +1,38 @@
+import { Image } from "react-bootstrap";
+import { useCart } from "../context/CartContext";
+
+export const PurchaseSummary = () => {
+
+    const { 
+        cartItems,
+        totalPriceCart
+    } = useCart();
+
+    return (
+        <div className="purchase-summary-container">
+            <h5>Resumen de compra</h5>
+            {
+                cartItems.map((
+                    {
+                        id,
+                        title,
+                        image,
+                        priceWithDiscount,
+                        quantity
+                    }
+                ) => (
+                    <div key={id} className="item-container">
+                        <div className="image-container">
+                            <Image src={image} style={{width: '80px'}} className="border rounded"/>
+                            <span className="item-quantity">{quantity}</span>
+                        </div>
+                        <strong className="item-title">{title}</strong>
+                        <span className="item-price">S/ {priceWithDiscount}</span>
+                    </div>
+                ))
+            }
+            <hr />
+            <div className="d-flex justify-content-between px-3"> <strong>Total</strong> <strong>S/ {totalPriceCart}</strong></div>
+        </div>
+    )
+}
