bb83b893b39d2654456f5b5000b25046a059f8e9
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 17:07:52 -0500

    fix: Fix rendering issue for quantityItems condition

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,42 +1,40 @@
-import { Button } from "react-bootstrap";
-import { CartIcon } from './Icons';
-import { useCart } from "../context/CartContext";
+import { useCart } from "../context/CartContext"
+import { CustomIcon } from "./Icons";
 import { useNavigate } from "react-router-dom";
 
-export const CartWidget = ({ cartQuantity }) => {
+export const CartWidget = () => {
 
-    const { quantityItems } = useCart()
+    const { quantityItems } = useCart();
     const navigate = useNavigate();
 
     const goToCart = () => navigate(`/cart`);
+    
+
     return (
-
-        <>
-            { quantityItems > 0 && 
-                <button
-                className = "button"
-                onClick={goToCart}
-                style={{ position: "relative"}}
-
-            >
-                <CartIcon/>
-
-                <div
-                className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-                style={{
-                    color: "white",
-                    width: "1.2rem",
-                    height: "1.2rem",
-                    position: "absolute",
-                    top: "-0.5rem",
-                    right: "0rem",
-                    transform: "translate(25%, 25%)",
-                }}
-                >
-                {quantityItems}
-                </div>
-            </button>
+        <div
+            onClick={goToCart}
+            className="position-relative"
+        >
+            <CustomIcon name = "cart"/>
+            {
+                quantityItems > 0 && 
+                    <div
+                        className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                        style={{
+                            color: "white",
+                            width: "1.2rem",
+                            height: "1.2rem",
+                            position: "absolute",
+                            top: "-0.5rem",
+                            right: "-0.2rem",
+                            transform: "translate(25%, 25%)",
+                            fontSize: '12px',
+                            fontWeight: 'bolder'
+                        }}
+                    >
+                        {quantityItems}
+                    </div>
             }
-        </> 
+        </div>
     )
 }
