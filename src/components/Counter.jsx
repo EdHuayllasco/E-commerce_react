0404f8f61d48d13c3a69ff5552ef23b07fd8feb0
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 14:10:26 -0500

    Button to add items  to cart

---
+++ src/components/Counter.jsx
@@ -0,0 +1,30 @@
+
+export const Counter = ({quantity, setQuantity}) => {
+
+  
+
+    return (
+        <div
+            className="d-flex align-items-center justify-content-start counter"
+            
+        >
+            <button
+                className="counter-btn"
+                onClick={() => setQuantity(
+                    prevStatus => prevStatus == 1 
+                        ? 1
+                        : prevStatus - 1)}
+                >
+                -
+            </button>
+                <span>{quantity}</span> 
+            <button
+                className="counter-btn"
+                onClick={() => setQuantity(quantity + 1)}
+                >
+                +
+            </button>
+
+        </div>
+    )
+}
