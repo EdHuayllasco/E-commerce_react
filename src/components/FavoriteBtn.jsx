4e0d92c0c2bc627e1395f7282360a63387b30fc2
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 17:47:00 -0500

    Style: Change color

--- src/components/FavoriteBtn.jsx
+++ src/components/FavoriteBtn.jsx
@@ -6,31 +6,30 @@ import { favoriteProduct } from "../utils/favorites";
 
 export const FavoriteBtn = ({ productId }) => {
   
-  // const { user, favorites } = useAuth();
-  // const product = favoriteProduct(productId, favorites) ;
+  const { user, favorites } = useAuth();
+  const product = favoriteProduct(productId, favorites) ;
 
-  // const [active, setActive ] = useState(product ? true: false);
+  const [active, setActive ] = useState(product ? true: false);
 
-  // const handleActive = (e) => {
-  //   e.stopPropagation();
+  const handleActive = (e) => {
+    e.stopPropagation();
     
-  //   if(active) {
-  //     deleteFavoriteProduct(product?.id)
-  //   } else {
-  //     addFavoriteProduct(productId, user?.uid)
-  //   }
-  //   setActive(!active)
-  // }
+    if(active) {
+      deleteFavoriteProduct(product?.id)
+    } else {
+      addFavoriteProduct(productId, user?.uid)
+    }
+    setActive(!active)
+  }
 
   
   return (
-    // <button 
-    //   className='btn-favorite' 
-    //   onClick={handleActive}>
-    //     {
-    //       <CustomIcon size={18} name={active ? "favorite-fill": "favorite"} color="#0bc1a9"/>
-    //     }
-    // </button>
-    <></>
+    <button 
+      className='btn-favorite' 
+      onClick={handleActive}>
+        {
+          <CustomIcon size={18} name={active ? "favorite-fill": "favorite"} color="#48BFE7"/>
+        }
+    </button>
   )
 }
