4429800a358d510b0fe516758d66bdad21f5f54e
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 19:45:13 -0500

    feat: Add condition to show button that redirects to my ordes

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,22 +1,28 @@
 import { Button } from "react-bootstrap"
 import { useLocation, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext";
 
 
 export const Confirmation = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const { user } = useAuth();
 
   const {orderId} = location.state;
   return (
     <div className="text-center my-5">
         <h4>¡Confirmación de compra!</h4>
         <p>Felicidades su pedido con el <strong>Código: {orderId}</strong> se realizo con exito</p>
-        <p>Para saber el estado de sus pedidos haga click en el siguiente boton</p>
-        <Button
-          className = "bt main-btn" 
-          onClick={ () => navigate('/account',{state:{option:'orders'}})}>
-            Mis pedidos
-        </Button>
+        {
+          user && <>
+            <p>Para saber el estado de sus pedidos haga click en el siguiente boton</p>
+            <Button
+              className = "bt main-btn" 
+              onClick={ () => navigate('/account',{state:{option:'orders'}})}>
+                Mis pedidos
+            </Button>
+          </>
+        }
     </div>
   )
 }
