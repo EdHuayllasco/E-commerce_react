5d7fe435a828047e07b453f1696fee5aefd0747d
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-28 19:36:40 -0500

    feat: show succes message with the ID of the order created

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,13 +1,16 @@
 import { Button } from "react-bootstrap"
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 
 export const Confirmation = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const {orderId} = location.state;
   return (
     <div className="text-center my-5">
         <h4>¡Confirmación de compra!</h4>
-        <p>Felicidades su pedido se realizo con exito</p>
+        <p>Felicidades su pedido con el <strong>Código: {orderId}</strong> se realizo con exito</p>
         <p>Para saber el estado de sus pedidos haga click en el siguiente boton</p>
         <Button
           className = "bt main-btn" 
