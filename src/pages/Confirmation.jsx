971121bcbcf877a703a03894f8d2615583050877
EdHuayllasco <edwardhc1997@gmail.com>
2023-09-29 21:14:34 -0500

    chore: add image in a message

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,20 +1,27 @@
-import { Button } from "react-bootstrap"
+import { Button, Image } from "react-bootstrap"
 import { useLocation, useNavigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext";
+import success from '../assets/images/completing.svg'
 
 
 export const Confirmation = () => {
   const navigate = useNavigate();
-  const location = useLocation();
+  const { state } = useLocation();
   const { user } = useAuth();
 
-  const {orderId} = location.state;
+
   return (
     <div className="text-center my-5">
         <h4>¡Confirmación de compra!</h4>
-        <p>Felicidades su pedido con el <strong>Código: {orderId}</strong> se realizo con exito</p>
+        <Image src={success} style={{
+          width: '200px'
+        }}
+        className="my-4"
+        />
+        <p>Felicidades su pedido con el <strong>Código: { state?.orderId } </strong> se realizo con exito</p>
         {
-          user && <>
+          user 
+          ? <>
             <p>Para saber el estado de sus pedidos haga click en el siguiente boton</p>
             <Button
               className = "bt main-btn" 
@@ -22,6 +29,7 @@ export const Confirmation = () => {
                 Mis pedidos
             </Button>
           </>
+          :<Button onClick={ () => navigate('/')}>Ir a productos</Button>
         }
     </div>
   )
